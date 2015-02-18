a95d495414e949290a63b3ae74eaf1bb9bb0217c
Guillermo Vargas <gavargas22@gmail.com>
2015-02-17 17:31:14 -0700

    The question system is now working with the Gem rapidfire.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
 
-  helper_method :current_user
+	helper_method :current_user
 
-  def current_user
-    #User.from_sso(cookies[:UTEP_SE], cookies[:UTEP_SA])
-    User.first
-  end
+	def current_user
+		#User.from_sso(cookies[:UTEP_SE], cookies[:UTEP_SA])
+		User.first
+	end
+
+	def can_administer?
+		true
+	end
 end
