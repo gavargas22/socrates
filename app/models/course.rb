ac4a1d53bd8acac23afe35cee509e007e3720075
Guillermo Vargas <gavargs22@gmail.com>
2015-02-17 23:53:40 -0800

    Some more changes to the sections

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,4 +7,9 @@ class Course < ActiveRecord::Base
   has_and_belongs_to_many :students
   has_secure_password
   mount_uploader :course_avatar, CourseAvatarUploader
+
+  def get_section_id
+    @section = Section.find(:id)
+    return "45"
+  end
 end
