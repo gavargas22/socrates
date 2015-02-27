5114192f8632e589a48b4fa2f1c2246f37d52c11
Alan Wernick <awernick@miners.utep.edu>
2015-02-27 13:30:29 -0700

    Start session logic and user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    redirect_to "https://adminapps.utep.edu/sso?redirectURL=#{create_session_url}"
+  end
+
+  # Create a new session using UTEPSSO
+  def create
+    user = User.from_sso(cookies[:UTEP_SE], cookies[:UTEP_SA])
+    log_in(user) if user
+    redirect_to root_url
+  end
+
+  # Terminate current session and deauthenticate from SSO system
+  def destroy
+    log_out
+    UTEPSSO.deauthenticate(cookies[:UTEP_SE], cookies[:UTEP_SA])
+    redirect_to root_url
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,26 @@
 class User < ActiveRecord::Base
-  #All the user information is located in the users table, load it
+  # All the user information is located in the users table, load it
   self.table_name = "users"
+
   # We obtain information from the SSO Login
+  # Return a User with SSO information
+	def self.from_sso(utep_cookie, utep_salt)
+		sso_response = UTEPSSO.authenticate(utep_cookie, utep_salt)
+
+		user ||= User.find_by(username: sso_response[:user_name])
+
+		unless user
+			user.name = sso_response[:full_name]
+			user.username = sso_response[:user_name]
+			user.slug = sso_response[:user_name]
+			user.email = sso_response[:email_address]
+			user.params = " "
+			user.save!
+
+			# mailboxer_name_var = sso_response[:username]
+			# mailboxer_email_var = sso_response[:email_address]
+		end
+
+		return user
+	end
 end
