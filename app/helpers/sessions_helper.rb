f3dc5afc5de637245ed90957c341d2e2a0734d80
Guillermo Vargas <gavargas22@gmail.com>
2015-03-02 19:06:36 -0700

    Some modifications to the header for index page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+  # Set current user from the current session
+  def current_user
+    # return User.first if Rails.env.development?
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Initiate a session for the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Check if a session exists and is valid in SSO
+  def logged_in?
+    # return true if Rails.env.development?
+    !current_user.nil? && UTEPSSO.authenticated?(cookies[:UTEP_SE], cookies[:UTEP_SA])
+  end
+
+  # Terminate current session and clear the current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
