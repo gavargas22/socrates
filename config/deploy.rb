b68fcfefa1ac968aac87ad0f047cb4f4fbfb2c35
Guillermo Vargas <gavargas22@gmail.com>
2015-04-04 00:26:53 -0600

    Capistrano branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, 'socrates'
 # set :repo_url, 'https://github.com/[user name]/[application name].git'
 set :scm, :git
 set :repo_url, 'git@git.at.utep.edu:gavargas/socrates.git'
+set :branch, fetch(:branch, "master")
 
 
 # Define where to put your application code
@@ -23,23 +24,23 @@ set :format, :pretty
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+	desc 'Restart application'
+	task :restart do
+		on roles(:app), in: :sequence, wait: 5 do
+			# Your restart mechanism here, for example:
+			execute :touch, release_path.join('tmp/restart.txt')
+		end
+	end
 
-  after :publishing, :restart
+	after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+			# Here we can do anything such as:
+			# within release_path do
+			#   execute :rake, 'cache:clear'
+			# end
+		end
+	end
 
 end
