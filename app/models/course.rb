eb7bfa1dc72aa6b59d65a6e1cbd250e6c5cbb76e
Guillermo Vargas <gavargas22@gmail.com>
2015-04-07 17:39:23 -0600

    Rough function of system.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,12 @@
 class Course < ActiveRecord::Base
   extend FriendlyId
+  # Use the CRN for easy URL access
   friendly_id :crn, use: :slugged
-
+  # Associations
   belongs_to :section
   has_one :faculty
   has_and_belongs_to_many :students
+
   has_secure_password
   # Course Avatar
   mount_uploader :course_avatar, CourseAvatarUploader
