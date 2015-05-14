a79b4a73d818c49057ba1f487949c9d93d5b6ddc
Guillermo Vargas <gavargas22@gmail.com>
2015-05-13 18:34:18 -0600

    fixes

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,14 +2,14 @@ class SubscriptionsController < ApplicationController
 
   # Subscribe current user to the given event
   def create
-    course = Course.friendly.find(params[:course_id])
+    course = Course.find(params[:course_id])
     current_user.subscribe(course)
     redirect_to course
   end
 
   # Unsubscribe current_user from the given event
   def destroy
-    course = Course.friendly.find(params[:id]).course
+    course = Course.find(params[:id]).course
     current_user.unsubscribe(course)
     redirect_to course
   end
